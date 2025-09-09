84105bac4b02d67efcd730809e950987270200ef
Aristeu-Miranda <aristeujose1999@gmail.com>
2025-09-08 21:05:37 -0300

    feat: add section challenge and objective

--- lib/projetc-data.ts
+++ lib/projetc-data.ts
@@ -5,41 +5,34 @@ export const ProjectHeader = {
 }
 
 export const ProjectAbout = {
-    AboutProject: {
         title: 'Sobre o Projeto',
-        description: 'A Mentor Cycle surgiu como uma iniciativa colaborativa e voluntária voltada ao desenvolvimento de profissionais iniciantes na área de UX/UI Design. O projeto foi idealizado para criar um ambiente de troca entre mentores experientes e designers em início de carreira, promovendo aprendizado prático, soft skills e inserção no mercado. Durante a sua execução, foram identificadas diversas dificuldades enfrentadas por iniciantes, como insegurança técnica, falta de direcionamento e ausência de vivência em times reais.',
-    },
+        description: 'A Mentor Cycle surgiu como uma iniciativa colaborativa e voluntária voltada ao desenvolvimento de profissionais iniciantes na área de UX/UI Design. O projeto foi idealizado para criar um ambiente de troca entre mentores experientes e designers em início de carreira, promovendo aprendizado prático, soft skills e inserção no mercado. Durante a sua execução, foram identificadas diversas dificuldades enfrentadas por iniciantes, como insegurança técnica, falta de direcionamento e ausência de vivência em times reais.'
+    }
 
-    Challenge: {
-        title: 'Desafio e objetivo',
-        challenge: {
+export const ProjectChallenge = {
+            titleSection: 'Desafio e Objetivo',
             title: 'Desafio',
-            description: 'Durante as primeiras entrevistas com potenciais mentorados, percebemos que muitos deles se sentiam perdidos não apenas tecnicamente, mas emocionalmente, por não saberem por onde começar. O sentimento de solidão na jornada e falta de direcionamento causavam desistências precoces. No lado dos mentores, identificamos dificuldades em organizar agendas, acompanhar o progresso dos mentorados e entender como realmente estavam contribuindo para o crescimento deles.',
+            description: 'Durante as primeiras entrevistas com potenciais mentorados, percebemos que muitos deles se sentiam perdidos não apenas tecnicamente, mas emocionalmente, por não saberem por onde começar. O sentimento de solidão na jornada e falta de direcionamento causavam desistências precoces. \nNo lado dos mentores, identificamos dificuldades em organizar agendas, acompanhar o progresso dos mentorados e entender como realmente estavam contribuindo para o crescimento deles.',
             tips: {
                 title: 'Principais Desafios:',
-                descriptions: [{
-                    description: 'Dificuldade em encontrar mentores compatíveis;',
-                }, {
-                    description: 'Falta de estrutura nas interações;',
-                }, {
-                    description: 'Baixo aproveitamento das sessões.',
-                }]
+                descriptions: [
+                    'Dificuldade em encontrar mentores compatíveis;',
+                    'Falta de estrutura nas interações;',
+                    'Baixo aproveitamento das sessões.'
+                ]
             }
-        }
-        },
-        objective: {
-            title: 'Nossa Solução',
-            descriptionOne: 'Para resolver a dor de orientação e progresso, criamos um sistema de trilha personalizada com marcos de aprendizagem, que ajudam mentor e mentorado a saber onde estão e o que precisam fazer em seguida.',
-            descriptionTwo: 'A agenda integrada com lembretes e acompanhamento dos encontros permitiu que os mentores tivessem mais controle do tempo investido e que os mentorados não perdessem o engajamento ao longo do processo.',
-            descriptionThree: 'Além do aprendizado técnico, o projeto ofereceu direcionamento sobre posicionamento no mercado, construção de networking, preparação para entrevistas e definição de metas profissionais. O objetivo foi impulsionar não apenas o conhecimento, mas também a autoconfiança e a clareza de trajetória dos participantes'
-        },
-        solution: {
-            descriptions: [{
-                description: 'Mentoria com profissionais da área;',
-            }, {
-                description: 'Apoio e desenvolvimento como profissional;',
-            }, {
-                description: 'Foco em evolução de carreira.',
-            }]
-        }
-    }
+}
+
+export const ProjectObjective = {
+    title: 'Nossa Solução',
+    descriptions: [
+        'Para resolver a dor de orientação e progresso, criamos um sistema de trilha personalizada com marcos de aprendizagem, que ajudam mentor e mentorado a saber onde estão e o que precisam fazer em seguida.',
+        'A agenda integrada com lembretes e acompanhamento dos encontros permitiu que os mentores tivessem mais controle do tempo investido e que os mentorados não perdessem o engajamento ao longo do processo.',
+        'Além do aprendizado técnico, o projeto ofereceu direcionamento sobre posicionamento no mercado, construção de networking, preparação para entrevistas e definição de metas profissionais. O objetivo foi impulsionar não apenas o conhecimento, mas também a autoconfiança e a clareza de trajetória dos participantes'
+    ],
+    solution: [
+        'Mentoria com profissionais da área;',
+        'Apoio e desenvolvimento como profissional;',
+        'Foco em evolução de carreira.'
+    ]
+}
